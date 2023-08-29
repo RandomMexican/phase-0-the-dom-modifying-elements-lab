7cef9bac910197026fcf7397427070a720d2b407
mariovaquero <mvaquero143@gmail.com>
2023-08-28 20:41:54 -0400

    Complete creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove();
+const element = document.getElementsByName("head");
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = `<h1>Mario is the champion</h>`
+newHeader.id = "victory"
+element.append(newHeader);
+
